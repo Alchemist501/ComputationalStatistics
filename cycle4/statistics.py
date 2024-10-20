d30744205d854cf9ebe1ecf85ee68e713a3010dd
Siya <siya123jc@gmail.com>
2024-10-20 23:49:05 +0530

    Updated

--- cycle4/statistics.py
+++ cycle4/statistics.py
@@ -1,24 +1,4 @@
+#Experiment4.6:= Calculate the average, variance and standard deviaƟon in Python using NumPy
 import numpy as np
-
-def calculate_statistics(array):
-    mean = np.mean(array)
-    variance = np.var(array)
-    std_deviation = np.std(array)
-    return mean, variance, std_deviation
-
-def input_array(prompt):
-    while True:
-        try:
-            array_input = input(prompt)
-            array = np.array(list(map(float, array_input.split())))
-            return array
-        except ValueError:
-            print("Invalid input. Please enter numeric values separated by spaces.")
-
-array = input_array("Enter an array of numbers (space-separated): ")
-mean, variance, std_deviation = calculate_statistics(array)
-
-print("Mean:", mean)
-print("Variance:", variance)
-print("Standard Deviation:", std_deviation)
-
+array = [1,2,3,4,5,6,7,8,9,10]
+print("Mean is "+str(np.mean(array))+", variance is "+str(np.var(array))+" and standard deviation is "+str(np.std(array)))
