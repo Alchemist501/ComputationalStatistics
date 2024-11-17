4b35664eb38d354b475eb877f6471c2757d2ff6f
Siya <siya123jc@gmail.com>
2024-11-17 22:06:14 +0530

    Updated

--- cycle3/Fibanocci.py
+++ cycle3/Fibanocci.py
@@ -1,19 +1,7 @@
+# Experiment 3.4:= Compute fibanocci numbers efficiently using binet formula with Numpy
+
 import numpy as np
-
-def fibonacci_binet(n):
-    # Define the golden ratio and sqrt(5)
-    phi = (1 + np.sqrt(5)) / 2
-    sqrt5 = np.sqrt(5)
-    
-    # Calculate the nth Fibonacci number using Binet's formula
-    fib_n = (phi**n - (1 - phi)**n) / sqrt5
-    
-    # Round the result to get the nearest integer
-    return round(fib_n)
-
-# Generate Fibonacci sequence up to the 10th term as an example
-n_terms = 10
-fibonacci_series = [fibonacci_binet(n) for n in range(n_terms)]
-
-print("Fibonacci Series using Binet's Formula:")
-print(fibonacci_series)
+def fibanocci(n):
+	phi = (1 + np.sqrt(5))/2
+	return int((phi**n - (-phi ** -n))/np.sqrt(5))
+print("Fibanocci numbers are : ",[fibanocci(n) for n in range(int(input("Enter the number of terms : ")))])
