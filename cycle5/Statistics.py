d4e866ce239b7b376d620bee2dc911e4d2806b4e
Siya <siya123jc@gmail.com>
2024-11-17 21:29:41 +0530

    Updated

--- cycle5/Statistics.py
+++ cycle5/Statistics.py
@@ -1,19 +1,7 @@
-#Experiment5.3:= Table contains population and murder rates (in units of murders per 100,000 people per year) for different states. Compute the mean, median and variance for the population.
-import statistics as stat
-data =  [
-    ["Alabama", 4779736, 5.7],
-    ["Alaska", 710231, 5.6],
-    ["Arizona", 6392017, 4.7],
-    ["Arkansas", 2915918, 5.6],
-    ["California", 37253956, 4.4],
-    ["Colorado", 5029196, 2.8],
-    ["Connecticut", 3574097, 2.4],
-    ["Delaware", 897934, 5.8]
-]
-ls = []
-for row in data:
-	ls.append(row[1])
-print(str(stat.mean(ls))+" is the mean of the population , "
-	+str(stat.median(ls))+" is the median and "
-	+str(stat.variance(ls))+" is the variance"
-)
+# Experiment5.3:= Table contains population and murder rates (in units of murders per 100,000 people per year) for different states. Compute the mean, median and variance for the population.
+import statistics as st
+
+data = [4779736, 710231, 6392017, 2915918, 37253956, 5029196, 3574097, 897934]
+print("Mean is : ", st.mean(data))
+print("Median is : ", st.median(data))
+print("Variance is : ", st.variance(data))
